feb505a1e1615c7d83cd52c1f0fd7afd35cfa5d0
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2024-12-19 23:34:08 +0600

    Delete 4.py

--- 4.py
+++
@@ -1,8 +0,0 @@
-
-set1 = {1,2,3,4,5,6,7,8}
-set2 = {6,7,4,8,9,10}
-
-union_set = set1.union(set2)
-intersection_set = set1.intersection(set2)
-print(f"This union is {union_set} and length of this set is {len(union_set)}")
-print(f"This intersection is {intersection_set} and length of this set is {len(intersection_set)}")
