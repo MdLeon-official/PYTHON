bd3e37c9a4a5feaa2abd72558f961ee471522b1b
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2024-12-19 23:34:40 +0600

    Delete 7_subject.py

--- 7_subject.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter total subject: "))
-sum = 0
-for _ in range(n):
-    marks = int(input("Enter mark: "))
-    sum = sum + marks;
-
-print(f"Total marks: {sum}")
-print(f"Average: {sum/n}")
-print(f"Percentage: {(sum/n):.2f}%")
