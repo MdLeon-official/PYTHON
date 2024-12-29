4aea1569cee37bedafea55f7fdf4398d8c4b79db
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2024-12-29 10:38:16 +0600

    Update deep_thought.py

--- cs50p_tasks/week_1/deep_thought.py
+++ cs50p_tasks/week_1/deep_thought.py
@@ -1,1 +1,8 @@
-
+ans = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+list = [42, 'forty-two','Forty-two','forty two','FoRty TwO'," 42 "]
+if ans == str(42) or ans == "forty-two" or ans == "Forty-two" or ans == "FoRty TwO" or ans == " 42 " or ans == 'forty two':
+    print("yes")
+elif "42" in ans:
+    print("yes")
+else:
+    print("no")
