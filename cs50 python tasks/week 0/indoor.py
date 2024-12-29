f25f2184a565e342657909df24fdb891356064d3
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2024-12-29 10:28:02 +0600

    Create indoor.py

---
+++ cs50 python tasks/week 0/indoor.py
@@ -0,0 +1,10 @@
+message = input()
+
+if message == "HELLO":
+    print("hello")
+elif message == "HELLO, WORLD":
+    print("hello, world")
+elif message == "THIS IS CS50":
+    print("this is cs50")
+else:
+    print(message)
